d95aab9eba7334f27d669985e2704e054eee2e76
Gabriel-Gramaq <gabrielluiz-back@hotmail.com>
2022-08-10 17:13:00 -0300

    Membros estáticos

---
+++ section04/046_Membros_Estaticos/Produto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _046_Membros_Estaticos
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProduto(int quantity)
+        {
+            Quantidade -= quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}," +
+                $" Preço: ${Preco.ToString("F2", CultureInfo.InvariantCulture)}," +
+                $" Quantidade: {Quantidade}," +
+                $" Total em estoque: ${ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+    }
+}
