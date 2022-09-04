abd31d305e3d31742868c1d0760a7ea5ac0d15f8
gabrielback12 <gabrielluiz-back@hotmail.com>
2022-09-03 23:23:30 -0300

    076-Listas

---
+++ section04/cpf/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cpf
+{
+    internal class Pessoa
+    {
+        private string nome;
+        private string cpf;
+
+        public string VerificarCpf(string cpf)
+        {
+            return cpf.Substring(1,6);
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Cpf { get => cpf; set => cpf = value; }
+    }
+}
---
+++ section04/cpf/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using cpf;
+
+Pessoa pessoa = new Pessoa();
+
+Console.Write("Digite o cpf da pessoa: ");
+pessoa.Cpf = "063.169.969-42";
+
+Console.WriteLine();
+
+pessoa.Cpf = pessoa.Cpf.Replace(".", "").Replace("-", "");
+
+int[] cpfValido = new int[] {0,6,3,1,6,9,9,6,9,4,2};
+
+Console.WriteLine($"Cpf digitado = {cpfValido[0]*5}");
+
